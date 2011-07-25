478cc7b380bb1a887c791cd304c364d11fae6f91
Mauricio Fernandez <mfp@acm.org>
2011-07-25 16:31:54 +0200

    Expand benchmarks.

--- test/benchmark.ml
+++ test/benchmark.ml
@@ -16,28 +16,81 @@ struct
     ((t.s0 lsl 16) + (t.s1 land 0xFFFF)) land 0x3FFFFFFF
 end
 
-let loop_cost n =
-  let r = FRAND.make () in
+let time f =
   let t0 = Unix.gettimeofday () in
-    for i = 1 to n do
-      let k = FRAND.int r in
-        ignore (string_of_int k);
-        ignore (string_of_int i)
-    done;
-    (Unix.gettimeofday () -. t0)
+    f ();
+    Unix.gettimeofday () -. t0
+
+let loop_kv_cost n =
+  let r = FRAND.make () in
+    time
+      (fun () ->
+         for i = 1 to n do
+           let k = FRAND.int r in
+             ignore (string_of_int k);
+             ignore (string_of_int i)
+         done)
+
+let loop_k_cost n =
+  let r = FRAND.make () in
+    time
+      (fun () ->
+         for i = 1 to n do
+           let k = FRAND.int r in
+             ignore (string_of_int k);
+         done)
+
+let bm_get db ?seed n =
+  let r = FRAND.make ?seed () in
+  let dt =
+    time
+      (fun () ->
+         for i = 1 to n do
+           let k = FRAND.int r in
+             ignore (LDB.get db (string_of_int k))
+         done)
+  in dt -. loop_k_cost n
+
+let bm_iter_value db ?seed n =
+  let r = FRAND.make ?seed () in
+  let dt =
+    time
+      (fun () ->
+         let it = LDB.Iterator.make db in
+         let v = ref "" in
+           for i = 1 to n do
+             let k = FRAND.int r in
+             let key = string_of_int k in
+               LDB.Iterator.seek it key 0 (String.length key);
+               ignore (LDB.Iterator.value it v)
+             done;
+           LDB.Iterator.close it)
+  in dt -. loop_k_cost n
+
+let bm_put db ?seed n =
+  let r = FRAND.make () in
+  let dt =
+    time
+      (fun () ->
+         for i = 1 to n do
+           let k = FRAND.int r in
+             LDB.put db (string_of_int k) (string_of_int i)
+         done)
+  in dt -. loop_kv_cost n
 
 let () =
-  let db = LDB.open_db "/tmp/ldb" in
-  let r = FRAND.make () in
   let n = 1_000_000 in
+  let seed = 0 in
 
-  let t0 = Unix.gettimeofday () in
-    for i = 1 to n do
-      let k = FRAND.int r in
-        LDB.put db (string_of_int k) (string_of_int i)
-    done;
-    LDB.close db;
-    let dt = Unix.gettimeofday () -. t0 in
-    let dt0 = loop_cost n in
-      printf "Needed %8.5fs (empty: %8.5fs)\n" dt dt0
+    List.iter
+      (fun (op, f) ->
+         let db = LDB.open_db "/tmp/ldb" in
+         let dt = f db ?seed:(Some seed) n in
+           printf "%20s  %7d/s\n%!" op (truncate (float n /. dt));
+           LDB.close db)
+      [
+        "put", bm_put;
+        "get", bm_get;
+        "Iterator.value", bm_iter_value;
+      ]
 
