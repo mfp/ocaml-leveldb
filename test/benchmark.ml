8b7d4b3ee85c1cabcb47eee93ef27577ed1842eb
Mauricio Fernandez <mfp@acm.org>
2011-07-25 17:20:15 +0200

    Benchmark sync put, batch put, seq and rev scan.

--- test/benchmark.ml
+++ test/benchmark.ml
@@ -67,30 +67,83 @@ let bm_iter_value db ?seed n =
            LDB.Iterator.close it)
   in dt -. loop_k_cost n
 
-let bm_put db ?seed n =
+let bm_put_aux ~sync db ?seed n =
   let r = FRAND.make () in
   let dt =
     time
       (fun () ->
          for i = 1 to n do
            let k = FRAND.int r in
-             LDB.put db (string_of_int k) (string_of_int i)
+             LDB.put ~sync db (string_of_int k) (string_of_int i)
          done)
   in dt -. loop_kv_cost n
+
+let bm_put = bm_put_aux ~sync:false
+let bm_put_sync = bm_put_aux ~sync:true
+
+let bm_batch_put_sync db ?seed n =
+  let r = FRAND.make () in
+  let dt =
+    time
+      (fun () ->
+         for i = 1 to n / 1000 do
+           let b = LDB.Batch.make () in
+           let m = i * 1000 in
+             for j = 0 to 1000 do
+               let i = m + j in
+               let k = FRAND.int r in
+                 LDB.Batch.put b (string_of_int k) (string_of_int i)
+             done;
+             LDB.Batch.write db ~sync:true b
+         done)
+  in dt -. loop_kv_cost n
+
+let bm_iter_scan_aux init next db ?seed n =
+  let dt =
+    time
+      (fun () ->
+         let it = LDB.Iterator.make db in
+         let k = ref "" in
+         let v = ref "" in
+           init it;
+           while LDB.Iterator.valid it do
+             ignore (LDB.Iterator.key it k);
+             ignore (LDB.Iterator.value it v);
+             next it;
+           done)
+  in dt
+
+let bm_iter_seq_scan =
+  bm_iter_scan_aux LDB.Iterator.seek_to_first LDB.Iterator.next
+
+let bm_iter_rev_scan =
+  bm_iter_scan_aux LDB.Iterator.seek_to_last LDB.Iterator.prev
+
+let downscale factor f db ?seed n =
+  f db ?seed (n / factor) *. float factor
 
 let () =
   let n = 1_000_000 in
   let seed = 0 in
 
     List.iter
-      (fun (op, f) ->
-         let db = LDB.open_db "/tmp/ldb" in
-         let dt = f db ?seed:(Some seed) n in
-           printf "%20s  %7d/s\n%!" op (truncate (float n /. dt));
-           LDB.close db)
+      (function
+           `O (op, f) ->
+             let db = LDB.open_db "/tmp/ldb" in
+             let dt = f db ?seed:(Some seed) n in
+               printf "%20s  %7d/s\n%!" op (truncate (float n /. dt));
+               LDB.close db
+         | `Clear -> ignore (LDB.destroy "/tmp/ldb"))
       [
-        "put", bm_put;
-        "get", bm_get;
-        "Iterator.value", bm_iter_value;
+        `Clear;
+        `O ("put", bm_put);
+        `O ("get", bm_get);
+        `O ("Iterator.value", bm_iter_value);
+        `Clear;
+        `O ("sync put", downscale 50 bm_put_sync);
+        `Clear;
+        `O ("batch put", bm_batch_put_sync);
+        `O ("seq scan", bm_iter_seq_scan);
+        `O ("rev scan", bm_iter_rev_scan);
       ]
 
